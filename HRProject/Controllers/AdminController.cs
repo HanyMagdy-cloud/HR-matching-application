7b72d0d0f53d7a542445d9280c5dcc13afdce68c
Hany <honamagdy2@yahoo.com>
2025-11-29 15:14:07 +0100

    Dash borad

---
+++ HRProject/Controllers/AdminController.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using HRProject.Data;
+using HRProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRProject.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: /Admin
+        public async Task<IActionResult> Index()
+        {
+            var model = new AdminDashboardViewModel
+            {
+                TotalUsers = await _userManager.Users.CountAsync(),
+                TotalCompetences = await _context.Competences.CountAsync(),
+                TotalUserCompetences = await _context.UserCompetences.CountAsync(),
+                TotalProjects = await _context.Projects.CountAsync(),
+                TotalProjectRequirements = await _context.ProjectRequirements.CountAsync()
+            };
+
+            return View(model); // Views/Admin/Index.cshtml
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Users()
+        {
+            var users = await _userManager.Users.ToListAsync();
+
+            var model = new List<UserWithRolesViewModel>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+
+                model.Add(new UserWithRolesViewModel
+                {
+                    UserId = user.Id,
+                    Email = user.Email,
+                    FullName = user.FullName,
+                    Roles = roles.ToList()
+                });
+            }
+
+            return View(model); // Views/Admin/Users.cshtml
+        }
+    }
+}
---
+++ HRProject/Models/UserWithRolesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace HRProject.Models
+{
+    public class UserWithRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public List<string> Roles { get; set; } = new();
+    }
+}
